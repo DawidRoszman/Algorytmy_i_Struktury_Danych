9b95c6715b089004b176c077ee5c61ac2a18d232
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-03-29 12:00:21 +0200

    cd nodes

--- lab4/nodes.py
+++ lab4/nodes.py
@@ -7,32 +7,43 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.none = Node(None)
 
-    def listInsert(self, x):
-        # wstawia wezel x do listy L
-        # lista dwukierunkowa niecykliczna bez wartownika
-        x.next = self.head
-        if self.head is not None:
-            self.head.prev = x
-        self.head = x
-        x.prev = None
-
-    def listDelete(self, x):
-        # usuwa wezel x z listy
-        # lista dwukierunkowa niecykliczna bez wartownika
-        if x.prev is not None:
-            x.prev.next = x.next
+    def list_insert(self, x):
+        x.prev = self.none
+        if self.none.next is not None:
+            self.none.next.prev = x
+            x.next = self.none.next
         else:
-            self.head = x.next
-        if x.next is not None:
-            x.next.prev = x.prev
+            x.next = self.none
+        self.none.next = x
 
-    def listSearch(self, k):
-        # szuka wezla zawierajacego klucz k
-        # lista dwukierunkowa cykliczna z wartownikiem
+    def list_delete(self, x):
+        x.prev.next = x.next
+        x.next.prev = x.prev
+
+    def list_search(self, k):
         x = self.none.next
-        while x is not None and x.key != k:
+        while x is not None and x.key != k and x != self.none:
             x = x.next
-        return x  # wynik “none” oznacza, ze szukanego klucza
-        # nie ma na lisciev
+        return "No element in list" if x is self.none else x
+
+    def print_list(self):
+        if not self.none.next:
+            print("List is empty")
+            return
+        curr = self.none.next
+        while curr != self.none:
+            print(curr.key, curr.prev.key, curr.next.key)
+            curr = curr.next
+        print("End of List")
+
+
+node = Node(5)
+list = LinkedList()
+list.list_insert(Node(1))
+list.list_insert(Node(2))
+list.list_insert(Node(3))
+list.list_insert(Node(4))
+list.list_insert(node)
+list.print_list()
