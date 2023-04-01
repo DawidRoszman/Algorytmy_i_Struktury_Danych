148f62e50d5db7f74b749b06396fdcd87f364232
Dawid Roszman <d.roszman.682@studms.ug.edu.pl>
2023-04-01 16:01:51 +0200

    finished lab4

--- lab4/nodes.py
+++ lab4/nodes.py
@@ -16,6 +16,7 @@ class LinkedList:
             x.next = self.none.next
         else:
             x.next = self.none
+            self.none.prev = x
         self.none.next = x
 
     def list_delete(self, x):
@@ -26,7 +27,7 @@ class LinkedList:
         x = self.none.next
         while x is not None and x.key != k and x != self.none:
             x = x.next
-        return "No element in list" if x is self.none else x
+        return False if x is self.none else x
 
     def print_list(self):
         if not self.none.next:
@@ -34,16 +35,40 @@ class LinkedList:
             return
         curr = self.none.next
         while curr != self.none:
-            print(curr.key, curr.prev.key, curr.next.key)
+            print(curr.key)
             curr = curr.next
         print("End of List")
 
+    def copy_without_repeated_elements(self):
+        new_list = LinkedList()
+        curr = self.none.prev
+        while curr != self.none:
+            if not new_list.list_search(curr.key):
+                new_list.list_insert(Node(curr.key))
+            curr = curr.prev
+        return new_list
 
-node = Node(5)
+    def merge_to_new_list(self, list2):
+        new_list = LinkedList()
+        curr = self.none.prev
+        while curr != self.none:
+            new_list.list_insert(Node(curr.key))
+            curr = curr.prev
+        curr = list2.none.prev
+        while curr != list2.none:
+            new_list.list_insert(Node(curr.key))
+            curr = curr.prev
+        return new_list
+
+
 list = LinkedList()
 list.list_insert(Node(1))
 list.list_insert(Node(2))
 list.list_insert(Node(3))
-list.list_insert(Node(4))
-list.list_insert(node)
+second_list = LinkedList()
+second_list.list_insert(Node(4))
+second_list.list_insert(Node(5))
+second_list.list_insert(Node(6))
 list.print_list()
+list.copy_without_repeated_elements().print_list()
+list.merge_to_new_list(second_list).print_list()
